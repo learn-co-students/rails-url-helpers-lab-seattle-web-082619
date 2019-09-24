6c852afe22f07a87668f2ec4ff57c2b0daff4569
ianhunterharold <ianhunterharold@gmail.com>
2019-09-24 14:38:15 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :students 
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
+  # get '/students/:id/activate', to:'students@activate' , as: 'activate_student'
+  # http://guides.rubyonrails.org/routing.html   
 end
