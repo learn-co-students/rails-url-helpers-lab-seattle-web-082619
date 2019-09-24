b37037b10cfff56e085ce43aa3f0068d78756fff
renburnett <renardburnett@gmail.com>
2019-09-24 15:02:51 -0700

    implemented activate feature, edit feature and new feature

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,10 +6,35 @@ class StudentsController < ApplicationController
   end
 
   def show
+    #@student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name], active: params[:student][:active])
+    redirect_to students_path
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(first_name: params[:student][:first_name], last_name: params[:student][:last_name], active: params[:student][:active])  
+    redirect_to students_path
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy()
+    redirect_to students_path
   end
 
   private
-
     def set_student
       @student = Student.find(params[:id])
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #resources gets all default routes
+  resources :students
+  #can add custom route like `activate`
+  #get "students/:id/activate", to: "students#activate"
+
 end
